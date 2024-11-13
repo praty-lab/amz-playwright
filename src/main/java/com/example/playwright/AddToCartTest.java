ab5e8ffdc7494afde5580d3ba2f96fab682bf88a
praty-lab <pratheeshnandanam@gmail.com>
2024-11-13 16:17:24 +0530

    done basic implementation (#4)

--- src/main/java/com/example/playwright/AddToCartTest.java
+++ src/main/java/com/example/playwright/AddToCartTest.java
@@ -11,6 +11,9 @@ import io.qameta.allure.Story;
 
 public class AddToCartTest extends BaseTest {
 
+	
+	
+	
 	@Parameters("browserType")
 	@Test(priority = 4, dependsOnMethods = "com.example.playwright.LoginTest.loginTest", description = "Select category from dropdown, search for a product and add to cart") // Ensure																													// first
 	@Story("Search for a product add it to cart")
